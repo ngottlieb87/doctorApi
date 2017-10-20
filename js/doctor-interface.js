83823effe57a10cc82fdef0525ca480f1575ba2b
ngottlieb87 <nogpdx@gmail.com>
2017-10-20 11:23:46 -0700

    display doctors first and last name depending on ailment input

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,9 +3,9 @@ import { Doctor } from './../js/doctor.js';
 $(document).ready(function(){
   $("#form").submit(function(){
     event.preventDefault();
-    $('#ouput').text('');
+    $('#output').text('');
     let ailment = $('#ailment').val();
     let doctor = new Doctor;
-    doctor.apiCall(ailment);
+    let searchAilment = doctor.apiCall(ailment);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -5,7 +5,7 @@ export class Doctor{
   apiCall(ailment){
     let promise = new Promise(function(resolve,reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=broken%20bone&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
       request.onload = function(){
         if(this.status === 200){
           resolve(request.response);
@@ -18,13 +18,15 @@ export class Doctor{
     });
 
     promise.then(function(response){
-
-      let parsed = JSON.parse(response);
-
-      parsed.data.forEach(function(ailment){
-        $("#ouput").append(`<li><p>${ailment.first_name}</p></li>`)
+      let parsedAil = JSON.parse(response);
+      // debugger;
+      parsedAil.data.forEach(function(ailment){
+        $('#output').append(`<li><p>${ailment.profile.first_name} ${ailment.profile.last_name}</p></li>`);
+          // debugger;
       });
-    })
+    },
+    function(error){
+      $('#output').text(`There was an error with your search. Please try again.`);
+    });
   }
-
 }
