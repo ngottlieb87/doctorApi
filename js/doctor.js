6ef1c923ec0bd14628a79c5b26bd338fc14df968
ngottlieb87 <nogpdx@gmail.com>
2017-10-20 15:45:34 -0700

    add phone number for doctor

--- js/doctor.js
+++ js/doctor.js
@@ -33,7 +33,7 @@ export class Doctor{
   doctorSearch(name){
     let promise = new Promise(function(resolve,reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&skip=0&limit=50&user_key=${apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
       request.onload = function(){
         if(this.status === 200){
           resolve(request.response);
@@ -47,12 +47,14 @@ export class Doctor{
 
     promise.then(function(response){
       let parsedName = JSON.parse(response);
-      // debugger;
       parsedName.data.forEach(function(doctor){
         debugger;
-        $('#outputDoctor').append(`<li><p>${doctor.profile.first_name}  ${doctor.profile.last_name} ${doctor.profile.title}</p></li>
+        $('#outputDoctor').append(`<li><p>${doctor.profile.first_name} ${doctor.profile.last_name} ${doctor.profile.title}</p></li>
       <p>Accpeting New patients: ${doctor.practices[0].accepts_new_patients}</p>
-      <p>Address: ${doctor.practices[1].visit_address.street}   ${doctor.practices[1].visit_address.city}, ${doctor.practices[1].visit_address.state} ${doctor.practices[1].visit_address.zip}</p><p> Phone Number: ${doctor.practices[0].phones[0].number}`);
+      <p>Address: ${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}</p>
+      <p>Phone Number: ${doctor.practices[0].phones[0].number}</p>
+      <p>Website: ${doctor.practices[0].website}</p>`);
+
       });
     },
     function(error){
